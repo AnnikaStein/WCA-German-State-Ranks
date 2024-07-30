ba882043c3a718e717d5aa1cdb5260361467dc95
Annika Stein <annika-stein@t-online.de>
2024-07-30 19:39:26 +0200

    feat: algorithm that creates teams for statecup

--- python/statecup.py
+++ python/statecup.py
@@ -96,16 +96,176 @@ def sort_by_kinch(state_kinch_dataframe):
     sorted_df = state_kinch_dataframe.sort_values(by=['average_kinch'], ascending=[False])
     return sorted_df
 
-def create_state_info():
+# state level
+def get_state_avg_kinch(state_df, all_registered = False):
+    if all_registered:
+        return state_df[state_df['registered_at_comp'] == True].loc[:,
+                'average_kinch'].mean()
+    else:
+        return state_df[(state_df['registered_at_comp'] == True) & (state_df['willing_to_compete'] == True)].loc[:,
+                'average_kinch'].mean()
+
+# state level
+def create_state_teams(all_states_df, state_order, all_registered = False, debug = False):
+    # state_order shall be combined, pointing from back and from front
+    pairing_for_missing = {}
+    reverse_state_order = list(state_order.keys())[::-1]
+    for i, item in enumerate(state_order.keys()):
+        pairing_for_missing[item] = reverse_state_order[i]
+
+    # take the up to three best people from each state to form preliminary team
+    # this is the "first pass"
+    state_ids = state_order.keys()
+    assigned_names = {k : [] for k in state_ids}
+    n_assigned_names = {k : 0 for k in state_ids}
+    all_assigned_names = []
+    available_names = {k : [] for k in state_ids}
+    n_avail_names = {k : 0 for k in state_ids}
+    for k in state_ids:
+        state_df = all_states_df[k]
+        extra_avail = []
+        n_avail = 0
+        if all_registered:
+            n_in_state = len(state_df[state_df['registered_at_comp'] == True].index)
+            take_up_to = min(n_in_state, statecup_info.TEAM_SIZE)
+            preliminary_people = state_df[state_df['registered_at_comp'] == True]['name'].head(take_up_to).tolist()
+            if n_in_state > statecup_info.TEAM_SIZE:
+                extra_avail = state_df[state_df['registered_at_comp'] == True]['name'].tolist()[statecup_info.TEAM_SIZE:]
+                n_avail = len(extra_avail)
+        else:
+            n_in_state = len(state_df[(state_df['registered_at_comp'] == True) & (state_df['willing_to_compete'] == True)].index)
+            take_up_to = min(n_in_state, statecup_info.TEAM_SIZE)
+            preliminary_people = state_df[(state_df['registered_at_comp'] == True) & (state_df['willing_to_compete'] == True)]['name'].head(take_up_to).tolist()
+            if n_in_state > statecup_info.TEAM_SIZE:
+                extra_avail = state_df[(state_df['registered_at_comp'] == True) & (state_df['willing_to_compete'] == True)]['name'].tolist()[statecup_info.TEAM_SIZE:]
+                n_avail = len(extra_avail)
+        if debug:
+            print(preliminary_people)
+        available_names[k] = extra_avail
+        n_avail_names[k] = n_avail
+        assigned_names[k] = preliminary_people
+        n_assigned_names[k] = take_up_to
+        all_assigned_names = all_assigned_names + preliminary_people
+
+    if debug:
+        print('>> Done with first pass.')
+        print('>> available_names', available_names)
+        print()
+        print('>> n_avail_names', n_avail_names)
+        print()
+        print('>> assigned_names', assigned_names)
+        print()
+        print('>> n_assigned_names', n_assigned_names)
+        print()
+        print('>> all_assigned_names', all_assigned_names)
+        print()
+
+    # filling up missing spots
+    # this is the "second pass"
+    for m in pairing_for_missing.keys():
+        needed_n = statecup_info.TEAM_SIZE - n_assigned_names[m]
+        if needed_n > 0:
+            # needs extra people
+            # find the corresponding pair state
+            pair_with = pairing_for_missing[m]
+            if n_avail_names[pair_with] > needed_n:
+                # grab the first needed_n names
+                working_with = available_names[pair_with][:needed_n]
+                available_names[pair_with] = available_names[pair_with][needed_n:]
+                n_avail_names[pair_with] = len(available_names[pair_with])
+            elif n_avail_names[pair_with] <= needed_n:
+                # grab the available names
+                working_with = available_names[pair_with]
+                available_names[pair_with] = []
+                n_avail_names[pair_with] = 0
+            assigned_names[m] += working_with
+            n_assigned_names[m] = len(assigned_names[m])
+            all_assigned_names = all_assigned_names + working_with
+
+    if debug:
+        print('>> Done with second pass.')
+        print('>> available_names', available_names)
+        print()
+        print('>> n_avail_names', n_avail_names)
+        print()
+        print('>> assigned_names', assigned_names)
+        print()
+        print('>> n_assigned_names', n_assigned_names)
+        print()
+        print('>> all_assigned_names', all_assigned_names)
+        print()
+
+    # filling up missing spots
+    # this is the "third pass"
+    all_still_avail = sum(available_names.values(), [])
+    for m in pairing_for_missing.keys():
+        needed_n = statecup_info.TEAM_SIZE - n_assigned_names[m]
+        if needed_n > 0:
+            # needs extra people
+            # grab the first needed_n names
+            working_with = all_still_avail[:needed_n]
+            all_still_avail = all_still_avail[needed_n:]
+            assigned_names[m] += working_with
+            n_assigned_names[m] = len(assigned_names[m])
+            all_assigned_names = all_assigned_names + working_with
+
+    if debug:
+        print('>> Done with third pass.')
+        print('>> assigned_names', assigned_names)
+        print()
+        print('>> n_assigned_names', n_assigned_names)
+        print()
+        print('>> all_assigned_names', all_assigned_names)
+        print()
+    return assigned_names
+
+# using all available info and the algorithm
+def create_state_info(for_testing_only = False, deb = False):
     # to compare against, country level
     relevant_NRs = get_relevant_NRs()
     # people, competition level
     registered_returners = get_registered_returners()
     other_interested = statecup_info.CUSTOM_NEWCOMERS
-    all_info_for_html = {}
+    all_states = {}
+    states_by_mean_avg_kinch = {st : 0.0 for st in info.name_state.keys()}
     for st in info.id_state.keys():
         state_kinch_df = sort_by_kinch(create_state_kinch_df(st,
                                         registered_returners,
                                         relevant_NRs))
-        all_info_for_html[st] = [state_kinch_df, other_interested[st]]
-    return all_info_for_html
+        for o in other_interested[st]:
+            new_row = pd.Series({'average_kinch' : 0.0,
+                                    'wca_id' : 'Newcomer',
+                                    'name' : o,
+                                    'registered_at_comp' : True,
+                                    'willing_to_compete' : True})
+            state_kinch_df = pd.concat([state_kinch_df, new_row.to_frame().T],
+                ignore_index=True)
+
+        state_mean_avg_kinch = get_state_avg_kinch(state_kinch_df,
+                                                   all_registered = for_testing_only)
+        states_by_mean_avg_kinch[st] = state_mean_avg_kinch
+
+        all_states[st] = state_kinch_df
+
+    # we now know the overall state level (how good a state is on average)
+    # that way we can sort the states by their allrounder quality
+    # the first entry has the lowest mean kinch, last entry is best
+    sorted_states = dict(sorted(states_by_mean_avg_kinch.items(),
+                                key=lambda item: item[1]))
+    if deb:
+        print(sorted_states)
+
+    # now build the proposed team composition,
+    # from registered (+ willing) competitors
+    state_teams = create_state_teams(all_states,
+                                     sorted_states,
+                                     all_registered = for_testing_only,
+                                     debug = deb)
+
+    return all_states, state_teams
+
+if __name__ == "__main__":
+    print("Testing statecup functionality independently.")
+    main_func_output_scores, main_func_output_teams = create_state_info(for_testing_only = True,
+                                                                        debug = True)
+    print(main_func_output_scores, '\n\n', main_func_output_teams)
--- python/statecup_info.py
+++ python/statecup_info.py
@@ -1,12 +1,16 @@
 import info
 
 # parameters for custom kinch scores used to determine German State Cup participants
+TEAM_SIZE = 3
 EVENTS_IN_STATECUP = ['222', '333', '444', '333bf', '333oh', 'pyram', 'skewb', 'sq1', 'clock']
 FOR_COMPETITION = 'RubiksGermanNationals2024'
+# this will come from a google form that anyone willing to participate
+# needs to fill out and submit with their consent
 with open('../assets/list_ids/statecup_enthusiasts.txt', 'r') as file:
-    STATECUP_ENTHUSIASTS = [line for line in file]
+    STATECUP_ENTHUSIASTS = [line.rstrip('\n') for line in file]
 
 # hard-coding extra people (newcomers w/o WCA ID) who could participate
 # if their state is otherwise not filled
+# this comes from private or public messages directed to the responsible (AS)
 CUSTOM_NEWCOMERS = {st : [] for st in info.name_state.keys()}
 CUSTOM_NEWCOMERS['br'] = ['Emil Krause']
